eb74797f507593791c91a8351799f8a12d600373
Raihan Uddin Mobin <contact@mobin.dev>
2025-07-10 21:40:52 +0600

    practiced function

--- 2-go-essentials/profit_calculator.go
+++ 2-go-essentials/profit_calculator.go
@@ -8,12 +8,9 @@ import (
 func main() {
 	var revenue, expenses, taxRate float64
 
-	fmt.Print("Enter the revenue: ")
-	fmt.Scan(&revenue)
-	fmt.Print("Enter the expenses: ")
-	fmt.Scan(&expenses)
-	fmt.Print("Enter the tax rate (as a percentage): ")
-	fmt.Scan(&taxRate)
+	getPromptWithLabel("Enter the revenue: ", &revenue)
+	getPromptWithLabel("Enter the expenses: ", &expenses)
+	getPromptWithLabel("Enter the tax rate (as a percentage): ", &taxRate)
 
 	earningBeforeTax := revenue - expenses
 	profit := earningBeforeTax - (earningBeforeTax * taxRate / 100)
@@ -21,7 +18,19 @@ func main() {
 
 	fmt.Println("Profit Calculator")
 	fmt.Println("Earning Before Tax: $", earningBeforeTax)
-	fmt.Println("Earning After Tax: $", profit)
-	fmt.Println("Profit Ratio: ", math.Round(ratio*100)/100)
+	fmt.Printf("Earning After Tax: $%.2f\n", profit)
+	fmt.Printf("Profit Ratio: %.2f\n", ratio)
 	fmt.Println("Just testing math function ", math.Pow(2, 3))
+	fmt.Print(greet("Mobin"))
+}
+
+func getPromptWithLabel(labelName string, varName any) {
+	fmt.Print(labelName)
+	fmt.Scan(varName)
+}
+
+func greet(name string) (gs, bagGr string) {
+	gs = "Hello " + name + " Welcome to golang greet function ! \n"
+	bagGr = "Hello " + name + " You are A very bad guy!"
+	return gs, bagGr
 }
