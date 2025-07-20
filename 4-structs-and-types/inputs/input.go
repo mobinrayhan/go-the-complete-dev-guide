372aa6087fbc2a0866cd5f3830955cb119ea5dfe
Raihan Uddin Mobin <32673467+himonhimu@users.noreply.github.com>
2025-07-21 00:38:59 +0600

    remove sufix of new line

--- 4-structs-and-types/inputs/input.go
+++ 4-structs-and-types/inputs/input.go
@@ -5,12 +5,14 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func GetUserInput(label string) (string, error) {
 	fmt.Print(label, " : ")
 	reader := bufio.NewReader(os.Stdin)
 	userInput, err := reader.ReadString('\n')
+	userInput = strings.TrimSuffix(userInput, "/n")
 
 	if err != nil {
 		return "", errors.New("invalid user input")
